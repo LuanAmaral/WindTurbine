ac595492bcc31b96a5f16306edf3c02bd3c62cca
MarinaDaumas <53481929+MarinaDaumas@users.noreply.github.com>
2022-11-20 16:29:42 +0100

    read_data working version

--- read_wind_data.m
+++ read_wind_data.m
@@ -1,30 +1,8 @@
-T = readtable("wind_data_AnNinhDong.xlsx", "Range","A1:B500");  % skips the first three rows of date
+T = readtable("wind_data_AnNinhDong.xlsx"); 
 
-data_series1 = timeseries(T.Vavg80_m_s_);
+n_sample = 499;
+t_sample = 10*60;
 
-data_series2= timeseries(T.Vavg80_m_s_, T.Date_Time);
+time = 1:t_sample:t_sample*n_sample;
 
-% Convert to timetable
-T.Date_Time = datetime(T.Date_Time, 'InputFormat', 'dd/MM/yyyy HH:mm');
-T = table2timetable(T);
-
-% Create time vector (15 min duration)
-t = T.Date_Time(1);
-startTime = datetime(t.Year,t.Month,t.Day,t.Hour,t.Minute,t.Second);
-time = (startTime:minutes(10):T.Date_Time(end));
-
-% Resampling%T = retime(T,time,'linear');
-
-timest = datestr(time);
-%data_series2= timeseries(T.Vavg80_m_s_, timest);
-
-
-
-% t0=T.Date_Time(1);
-% tf=T.Date_Time(end);
-% 
-% dt=diff(t0)
-% t = t0:dt:tf;
-
-
-
+data_series = timeseries(T.Vavg80_m_s_(1:n_sample), time);
