24b7f9d6d516dc48e501cc70825b6205d471150d
MarinaDaumas <53481929+MarinaDaumas@users.noreply.github.com>
2022-11-23 21:01:29 +0100

    added data plot params

--- wind_turbine_parameters.m
+++ wind_turbine_parameters.m
@@ -11,3 +11,14 @@ mu = 50; %friction
 l2 = 47; %m
 m2 = 12474; %Kg
 J2 = kj * m2 * l1^2; 
+
+
+%Data plot params
+T = readtable("wind_data_AnNinhDong.xlsx"); 
+
+n_sample = 499;
+t_sample = 10*60;
+
+time = 1:t_sample:t_sample*n_sample;
+
+data_series = timeseries(T.Vavg80_m_s_(1:n_sample), time);
